d95e885def1e15cdc7defd6e8957ea1f022870e8
Enrico Sorio <enrico.sorio@gmail.com>
2016-10-24 16:36:47 +0200

    added support for Multi Monitors Add-On

--- transparenttopbar/extension.js
+++ transparenttopbar/extension.js
@@ -7,6 +7,11 @@ function init() {
 
 function enable() {
     // Add transparency
+    if (Main.mmPanel) {
+        for (var i = 0, len = Main.mmPanel.length; i < len; i++) {
+            Main.mmPanel[i].actor.add_style_class_name('panel-transparency');
+        }
+    }
     Main.panel.actor.add_style_class_name('panel-transparency');
     Main.panel._leftCorner.actor.add_style_class_name('corner-transparency');
     Main.panel._rightCorner.actor.add_style_class_name('corner-transparency');
@@ -14,6 +19,11 @@ function enable() {
 
 function disable() {
     // Restore opacity
+    if (Main.mmPanel) {
+        for (var i = 0, len = Main.mmPanel.length; i < len; i++) {
+            Main.mmPanel[i].actor.remove_style_class_name('panel-transparency');
+        }
+    }
     Main.panel.actor.remove_style_class_name('panel-transparency');
     Main.panel._leftCorner.actor.remove_style_class_name('corner-transparency');
     Main.panel._rightCorner.actor.remove_style_class_name('corner-transparency');
